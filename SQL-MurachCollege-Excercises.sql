e7c844e76f7e350f3e49ea330b51089f14346630
heath-steiger <heathsteiger@gmail.com>
2025-01-23 20:11:20 -0500

    innit commit

---
+++ SQL-MurachCollege-Excercises.sql
@@ -0,0 +1,14 @@
+--Excercise 1
+SELECT CourseDescription
+	FROM Courses;
+--Excercise 2
+Select COUNT(*) AS NumberOfInstructors
+	FROM Instructors;
+--Excercise 3
+SELECT LastName, FirstName, HireDate, AnnualSalary
+	FROM Instructors
+ORDER BY AnnualSalary;
+--Excercise 4
+SELECT LastName, FirstName, HireDate, AnnualSalary
+	FROM Instructors
+ORDER BY AnnualSalary;
