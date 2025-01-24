1f5146f30e8297b2d97db768103406f2afe67eee
heath-steiger <heathsteiger@gmail.com>
2025-01-24 11:01:10 -0500

    fixed typo

---
+++ SQL-MurachCollege-Exercises (2).sql
@@ -0,0 +1,14 @@
+--Exercise 1
+SELECT CourseDescription
+	FROM Courses;
+--Exercise 2
+Select COUNT(*) AS NumberOfInstructors
+	FROM Instructors;
+--Exercise 3
+SELECT LastName, FirstName, HireDate, AnnualSalary
+	FROM Instructors
+ORDER BY AnnualSalary;
+--Exercise 4
+SELECT LastName, FirstName, HireDate, AnnualSalary
+	FROM Instructors
+ORDER BY AnnualSalary;
