853ee27b0d64cd5a36d5e612ec90e1209406a561
heath-steiger <heathsteiger@gmail.com>
2025-01-23 20:09:52 -0500

    updated sql)

--- SQL-CHAPTER-3LABS.sql
+++ SQL-CHAPTER-3LABS.sql
@@ -6,15 +6,15 @@ SELECT InvoiceNumber AS Number,
        InvoiceTotal AS Total,
        PaymentTotal + CreditTotal AS Credits,
        InvoiceTotal - (PaymentTotal + CreditTotal) AS Balance
-FROM Invoices;
+	FROM Invoices;
 --Excercise 3
 SELECT VendorContactLName + ', ' + VendorContactFName AS [Full Name]
-FROM Vendors
+	FROM Vendors
 ORDER BY VendorContactLName, VendorContactFName;
 --Excercise 4
 SELECT InvoiceTotal, InvoiceTotal / 10 AS [10%],
        InvoiceTotal * 1.1 AS [Plus 10%]
-FROM Invoices
+	FROM Invoices
 WHERE InvoiceTotal - PaymentTotal - CreditTotal > 100
 ORDER BY InvoiceTotal DESC;
 --Excercise 5
@@ -22,15 +22,15 @@ SELECT InvoiceNumber AS Number,
        InvoiceTotal AS Total,
        PaymentTotal + CreditTotal AS Credits,
        InvoiceTotal - (PaymentTotal + CreditTotal) AS Balance
-FROM Invoices
+	FROM Invoices
 WHERE InvoiceTotal BETWEEN 500 AND 10000;
 --Excercise 6
 SELECT VendorContactLName + ', ' + VendorContactFName AS [Full Name]
-FROM Vendors
+	FROM Vendors
 WHERE VendorContactLName LIKE '[A,B,C,E]%'
 ORDER BY VendorContactLName, VendorContactFName;
 --Excercise 7
 SELECT *
-FROM Invoices
+	FROM Invoices
 WHERE ((InvoiceTotal - PaymentTotal - CreditTotal <= 0) AND
       PaymentDate IS NULL)
